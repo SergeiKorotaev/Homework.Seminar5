3e72a02825dcf67cfc8ac6412b8965a0d81ab14a
Sergei Korotaev <serega_korotaev24@gmail.ru>
2023-01-21 18:13:29 +0300

    Test3

---
+++ Test3/Program.cs
@@ -0,0 +1,36 @@
+﻿void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+            Console.WriteLine();
+}
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int j = 0; j < numbers.Length; j++)
+{
+    if (numbers[j] > max)
+        {
+            max = numbers[j];
+        }
+    if (numbers[j] < min)
+        {
+            min = numbers[j];
+        }
+}
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
